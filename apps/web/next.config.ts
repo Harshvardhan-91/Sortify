efe14b90f2f2985a025ad6a03374ea64c0b74d30
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2025-08-11 22:01:41 +0530

    Configured next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -7,6 +7,22 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
